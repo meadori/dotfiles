8b339d4de061cfb5d06f96403fe34985f11c1da9
Meador Inge <meadori@gmail.com>
2013-11-30 13:21:49 -0600

    Reformat the xmobar config file
    It seems as if all of the xmobar configuraiton has to be inlined into
    'Config' (e.g. no global variables), thus reindent the file to make
    it more readable.

--- config/xmonad/xmobar.hs
+++ config/xmonad/xmobar.hs
@@ -1,19 +1,23 @@
-Config { font = "xft:monospace-8"
-       , bgColor = "black"
-       , fgColor = "grey"
-       , position = Top
-       , lowerOnStart = True
-       , commands = [ Run Network "wlp3s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
-                    , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
-                    , Run Battery ["-t","<left>% / <timeleft>","-L","50","-H","75","-h","green","-n","yell","-l","red"] 10
-                    , Run Memory ["-t","Mem: <usedratio>%"] 10
-                    , Run Swap [] 10
-                    , Run Com "uname" ["-s","-r"] "" 36000
-                    , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
-                    , Run StdinReader
-                    ]
-       , sepChar = "%"
-       , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% >> %memory% * %swap% >> %wlp3s0% >> %battery% >> <fc=#ee9a00>%date%</fc> >> %uname%"
+Config {
+   font = "xft:monospace-8",
+   bgColor = "black",
+   fgColor = "grey",
+   position = Top,
+   lowerOnStart = True
+   commands = [
+      Run Network "wlp3s0" ["-L", "0", "-H", "32", "--normal",
+                            "green", "--high", "red"] 10,
+      Run Cpu ["-L", "3", "-H", "50", "--normal", "green", "--high", "red"] 10,
+      Run Battery ["-t", "<left>% / <timeleft>", "-L", "50", "-H", "75",
+                   "-h", "green", "-n", "yell", "-l", "red"] 10,
+      Run Memory ["-t", "Mem: <usedratio>%"] 10,
+      Run Swap [] 10,
+      Run Com "uname" ["-s", "-r"] "" 36000,
+      Run Date "%a %b %_d %Y %H:%M:%S" "date" 10,
+      Run StdinReader
+   ],
+   sepChar = "%",
+   alignSep = "}{",
+   template = "%StdinReader% }{ %cpu% >> %memory% * %swap% >> %wlp3s0% >> %battery% >> <fc=#ee9a00>%date%</fc> >> %uname%"
        }
 
